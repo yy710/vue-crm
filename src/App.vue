919be62ccc675bc5bb0840309e2c20683c76011a
Yu Chunjian <yuchunjian@Yus-MacBook-Pro.local>
2019-11-07 17:34:42 +0800

    first

--- src/App.vue
+++ src/App.vue
@@ -1,24 +1,43 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <router-link to="/111">1111</router-link>
+    <router-link to="/222">222</router-link>
+    <router-view></router-view>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Vue from "vue";
+import VueRouter from "vue-router";
+import HelloWorld from "./components/HelloWorld.vue";
+import NewReferred from "./components/NewReferred.vue";
+//const Bar = { template: "<div>bar</div>" };
+//const Foo = { template: "<div>foo</div>" };
+const routes = [
+  {
+    path: "/111",
+    component: HelloWorld
+  },
+  {
+    path: "/222",
+    component: NewReferred
+  }
+];
+
+Vue.use(VueRouter);
+
+const router = new VueRouter({ routes });
 
 export default {
-  name: 'app',
-  components: {
-    HelloWorld
-  }
-}
+  name: "app",
+  //components: { HelloWorld, NewReferred },
+  router
+};
 </script>
 
 <style>
 #app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  font-family: "Avenir", Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
---
+++ src/components/NewReferred.vue
@@ -0,0 +1,56 @@
+<template>
+  <div>
+    <van-cell-group>
+      <van-field
+        v-model="customerName"
+        required
+        label="客户姓名"
+        left-icon="contact"
+        placeholder="请输入客户姓名"
+      ></van-field>
+      <van-field
+        v-model="customerPhone"
+        required
+        label="客户手机号"
+        placeholder="请输入客户手机号"
+        left-icon="phone-o"
+      ></van-field>
+      <van-cell>
+        <van-button type="default" @click="sendNewReferred">send</van-button>
+      </van-cell>
+    </van-cell-group>
+  </div>
+</template>
+
+<script>
+import Vue from "vue";
+import Vant from "vant";
+import axios from "axios";
+import "vant/lib/index.css";
+
+Vue.use(Vant);
+
+export default {
+  name: "NewReferred",
+  data() {
+    return {
+      customerName: null,
+      customerPhone: null
+    };
+  },
+  props: {
+    employer: Object
+  },
+  methods: {
+    sendNewReferred() {
+      axios
+        .get("http://www.all2key.cn/oa.html")
+        .then(r => window.console.log("axios return: ", r))
+        .catch(err => window.console.log("error!", err));
+    }
+  }
+};
+</script>
+
+<style>
+</style>
